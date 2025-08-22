bdc950d31c42c6f7176fe2330a7d42e13394472b
Alessandro Sarchi <alessandro.sarchi02@gmail.com>
2025-08-22 18:25:42 +0200

    feat: add new classes to simplify to CNF process

--- src/logic_ast.py
+++ src/logic_ast.py
@@ -64,5 +64,38 @@ class Biconditional(Formula):
         left_str = str(self.left) if isinstance(self.left, Variable) else f"({self.left})"
         right_str = str(self.right) if isinstance(self.right, Variable) else f"({self.right})"
         return f"{left_str} ↔ {right_str}" 
-    
+
+@dataclass
+class Literal(Formula):
+    variable: str
+    negated: bool = False
+
+    def __str__(self) -> str:
+        if self.negated:
+            return f"¬{self.variable}"
+        else:
+            return self.variable
+
+@dataclass
+class Clause(Formula):
+    literals: list[Literal]
+
+    def __str__(self) -> str:
+        if not self.literals:
+            return "⊥"
+        if len(self.literals) == 1:
+            return str(self.literals[0])
+        return " ∨ ".join(str(lit) for lit in self.literals)
+
+@dataclass
+class CNFFormula(Formula):
+    clauses: list[Clause]
+
+    def __str__(self) -> str:
+        if not self.clauses:
+            return "⊤"
+        if len(self.clauses) == 1:
+            return str(self.clauses[0])
+        return " ∧ ".join(f"({clause})" for clause in self.clauses)
+
 
